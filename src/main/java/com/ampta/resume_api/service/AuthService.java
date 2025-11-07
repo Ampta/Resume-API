10def5212bd8f3fb10f6cb3fd647e986e3d1cc6e
nakanu <ampta77000@gmail.com>
2025-11-07 19:34:09 +0530

    register endpoint is added

---
+++ src/main/java/com/ampta/resume_api/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.ampta.resume_api.service;
+
+import com.ampta.resume_api.dto.AuthResponse;
+import com.ampta.resume_api.dto.RegisterRequest;
+
+public interface AuthService {
+    AuthResponse register(RegisterRequest request);
+}

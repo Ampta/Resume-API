ab45a06e1577a80fab1ebf84ad4aa2f16affae10
nakanu <ampta77000@gmail.com>
2025-11-09 07:35:56 +0530

    Resume endpoints are added

--- src/main/java/com/ampta/resume_api/service/FileUploadService.java
+++ src/main/java/com/ampta/resume_api/service/FileUploadService.java
@@ -8,4 +8,6 @@ import java.util.Map;
 public interface FileUploadService {
 
     Map<String, String> uploadSingleImage(MultipartFile file) throws IOException;
+
+    Map<String, String> uploadResumeImages(String id, Object principal, MultipartFile thumbnail, MultipartFile profileImage) throws IOException;
 }
